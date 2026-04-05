a90f4dcd1b1ace7fc56a4799cd3c2e9c4f1fd756
richard0509 <91534739+richard0509@users.noreply.github.com>
2026-04-05 22:44:13 +0800

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int old_color = image[sr][sc];
+        return helper(image, sr, sc, color, old_color);
+    }
+
+    vector<vector<int>> helper(vector<vector<int>>& image, int sr, int sc, int color, int old_color) {
+        int ROW = image.size();
+        int COL = image[0].size();
+
+        if (std::min(sr, sc) < 0 || sr == ROW || sc == COL || image[sr][sc] != old_color || image[sr][sc] == color) {
+            return image;            
+        }
+
+        image[sr][sc] = color;
+
+        image = helper(image, sr + 1, sc, color, old_color);
+        image = helper(image, sr - 1, sc, color, old_color);
+        image = helper(image, sr, sc + 1, color, old_color);
+        image = helper(image, sr, sc - 1, color, old_color);
+
+        return image;
+
+    }
+
+};

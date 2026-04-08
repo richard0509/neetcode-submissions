d62f7c3f46f4794378f91792e18a5af26be00e7a
richard0509 <91534739+richard0509@users.noreply.github.com>
2026-04-08 22:54:49 +0800

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int R = grid.size();
+        int C = grid[0].size();
+
+        std::vector<std::vector<int>> visit(R, std::vector<int>(R, 0));
+
+        std::queue<std::pair<int, int>> q;
+        int fruits = 0;
+        for (int i = 0; i < R; ++i) {
+            for (int j = 0; j < C; ++j) {
+                if (grid[i][j] == 2) {
+                    q.push(std::pair<int, int>(i, j));
+                } else if (grid[i][j] == 1) {
+                    fruits++;
+                }
+            }
+        }
+
+        if (fruits == 0) {
+            return 0;
+        }
+
+        int minute = 0;
+        while (!q.empty()) {
+            int q_size = q.size();
+            for (int i = 0; i < q_size; ++i) {
+                std::pair<int, int> p = q.front();
+                q.pop();
+                int r = p.first;
+                int c = p.second;
+
+                int dir[4][2] = {{r + 1, c}, {r - 1, c}, {r, c + 1}, {r, c- 1}};
+                for (int j = 0; j < 4; ++j) {
+                    int nr = dir[j][0];
+                    int nc = dir[j][1];
+
+                    if (nr < 0 || nc < 0 || nr >= R || nc >= C || grid[nr][nc] != 1) {
+                        continue;
+                    }
+
+                    q.push(std::pair<int, int>(nr, nc));
+                    grid[nr][nc] = 2;
+                    fruits--;
+                }
+
+            }
+            minute++;
+            if (fruits == 0) {
+                return minute;
+            }
+        }
+        return -1;
+    }
+
+};

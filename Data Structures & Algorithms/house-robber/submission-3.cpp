26b7f3629218b9042d516aa07a580f2d551890af
richard0509 <91534739+richard0509@users.noreply.github.com>
2026-04-12 18:27:32 +0800

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        std::vector<int> cache(nums.size(), -1);
+
+        return dfs(nums, 0, cache);
+    }
+
+    int dfs(std::vector<int>& nums, int index, std::vector<int>& cache) {
+        if (index >= nums.size()) {
+            return 0;
+        }
+
+        if (cache[index] != -1) {
+            return cache[index];
+        }
+
+        cache[index] = std::max(nums[index] + dfs(nums, index + 2, cache), dfs(nums, index + 1, cache));
+        return cache[index];
+
+    }
+};

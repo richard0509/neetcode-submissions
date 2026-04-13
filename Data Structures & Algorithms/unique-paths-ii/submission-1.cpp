276ba6f34cb6947fc4bad769fb3f544932cd2019
richard0509 <91534739+richard0509@users.noreply.github.com>
2026-04-13 20:43:55 +0800

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        int r = 0;
+        int c = 0;
+
+        std::vector<std::vector<int>> memo(m, std::vector<int>(n, -1));
+
+        return brute_memo(r, c, m, n, obstacleGrid, memo);
+    }
+
+    int brute_memo(int r, int c, int m, int n, std::vector<std::vector<int>>& obstacleGrid, std::vector<std::vector<int>>& memo) {
+        if (r == m || c == n || obstacleGrid[r][c] == 1) {
+            return 0;
+        }
+
+        if (memo[r][c] != -1) {
+            return memo[r][c];
+        }
+
+        if (r == m - 1 && c == n - 1) {
+            return 1;
+        }
+
+        memo[r][c] = brute_memo(r + 1, c, m, n, obstacleGrid, memo) + brute_memo(r, c + 1, m, n, obstacleGrid, memo);
+        return memo[r][c];
+    }
+
+};

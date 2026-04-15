5c313c518fdbfc3f09dfbc23ee21acd07526bea5
richard0509 <91534739+richard0509@users.noreply.github.com>
2026-04-15 17:36:25 +0800

    Add: maximum-sum-circular-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        int max = nums[0];
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum = 0;
+            for (int j = i; j < nums.size() + i; ++j) {
+                sum += nums[j % n];
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
+    }
+};
